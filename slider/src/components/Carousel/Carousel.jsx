1d61896da9049845d4fb76f4d62e8de959133d38
HamedMiri72 <hamedmiri72@gmail.com>
2024-12-21 15:44:06 +0000

    functionality

--- slider/src/components/Carousel/Carousel.jsx
+++ slider/src/components/Carousel/Carousel.jsx
@@ -1,16 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Carousel.css"
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 const Carousel = ({data}) => {
   console.log(data);
+
+  const [slide, setSlide] = useState(0);
+  const nextSlide = () => {
+    setSlide(slide === data.length -1 ? 0 : slide + 1)
+  }
+
+  const prevSlide = () => {
+    setSlide(slide === 0 ? data.length - 1 : slide -1)
+  }
+
+
   return (
     <div className='carousel'>
+      <ArrowBackIosIcon className='arrow arrow-left' onClick={prevSlide}/>
       {data.map((item, index) => {
         
-        return <img src={item.src} alt={item.alt} key={index} className='slide'/>
+        return <img src={item.src} alt={item.alt} key={index} className={slide === index ? "slide" : "slide slide-hidden"}/>
 
       })}
+
+      <ArrowForwardIosIcon className='arrow arrow-right' onClick={nextSlide}/>
+      <span className='indicators'>
+        {data.map((_, index) =>{
+
+          return <button key={index} onClick={null} className={slide === index ? "indicator" : "indicator indicator-inactive"}></button>
+
+        })}
+      </span>
     </div>
+    
   )
 }
 
